45dc364c2c0800187c9f500b4f92830601f57a56
javadyavari2025-afk <javad.yavari2025@gmail.com>
2026-09-10 11:11:54 +0330

    Update peer-qrcode-card.tsx

--- ui/src/features/share/components/peer-qrcode-card.tsx
+++ ui/src/features/share/components/peer-qrcode-card.tsx
@@ -25,7 +25,7 @@ export default function PeerQRCodeCard({ isLoading, qrCode }: QRCodeCardProps) {
 
       <CardContent className='flex flex-1 flex-col items-center justify-center gap-4'>
         {isLoading ? (
-          <Skeleton className='h-[280px] w-[280px] rounded-2xl' />
+          <Skeleton className='h-[180px] w-[180px] rounded-2xl' />
         ) : (
           <div
             onClick={handleToggleBlur}
@@ -35,9 +35,9 @@ export default function PeerQRCodeCard({ isLoading, qrCode }: QRCodeCardProps) {
             <img
               src={qrCode}
               alt='QR Code'
-              width={260}
-              height={260}
-              className={`h-[260px] w-[260px] rounded-xl transition-all duration-300 ${
+              width={168}
+              height={168}
+              className={`h-[168px] w-[168px] rounded-xl transition-all duration-300 ${
                 isBlurred ? 'blur-md' : 'blur-0'
               }`}
             />
